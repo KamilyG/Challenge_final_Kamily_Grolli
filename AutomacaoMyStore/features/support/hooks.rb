1a84c44b7ca387bb73f677db13f288b5c7a133a2
Kamily Grolli <kamily369@live.com>
2022-08-15 08:58:16 -0300

    ajuste do arquivo hooks

--- AutomacaoMyStore/features/support/hooks.rb
+++ AutomacaoMyStore/features/support/hooks.rb
@@ -1,6 +1,6 @@
 After do |scenario|
     if scenario.failed?
-        screenshot = Capybara.page.save_screenshot("reports/screenshot/sc.png")
+        screenshot = Capybara.page.save_screenshot("reports/screenshots/sc.png")
         attach(screenshot, 'image/png')
     end
 end
